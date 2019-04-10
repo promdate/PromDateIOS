7a05f3505fc885277a813ed5cc3859c32caf9958
olcaron <48328624+olcaron@users.noreply.github.com>
2019-04-10 10:10:28 -0400

    complete login and register pages

---
+++ PromDate_app/PromDate app/Controller/MainFeedViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MainFeedViewController.swift
+//  PromDate app
+//
+//  Created by Olivier Caron on 2019-04-09.
+//  Copyright © 2019 Olivier Caron. All rights reserved.
+//
+
+import UIKit
+
+class MainFeedViewController: UIViewController {
+    var userToken = ""
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
