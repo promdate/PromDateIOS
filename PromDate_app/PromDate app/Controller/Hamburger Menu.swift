fbb9cdb1f76ba430e4e1b3b23a782a62a1a1d7f6
Hamza <hkhan12@ocdsb.ca>
2019-04-15 10:58:02 -0400

    Hamburger menu created

---
+++ PromDate_app/PromDate app/Controller/Hamburger Menu.swift
@@ -0,0 +1,21 @@
+//
+//  Hamburger Menu.swift
+//  PromDate app
+//
+//  Created by Hamza Khan on 2019-04-15.
+//  Copyright © 2019 Olivier Caron. All rights reserved.
+//
+
+import Foundation
+class HamburgerMenuViewController: UITabBarController {
+    
+    @IBAction func logout(_ sender: Any) {
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+}
+    
+}
