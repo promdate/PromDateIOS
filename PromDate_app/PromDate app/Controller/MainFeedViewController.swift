eea6b203a2150785a1e1eb1cb039a2e7c4abd064
Hamza <hkhan12@ocdsb.ca>
2019-05-24 11:26:15 -0400

    Revert "Merge branch 'mainFeed_Branch' of https://github.com/promdate/PromDateIOS into mainFeed_Branch"
    This reverts commit 8bc9d207f5bffcf57b992b9a1f41d2046d318db1, reversing
    changes made to 47090a34fbecaaff5459d974eff44f7393df8705.

--- PromDate_app/PromDate app/Controller/MainFeedViewController.swift
+++ PromDate_app/PromDate app/Controller/MainFeedViewController.swift
@@ -48,12 +48,11 @@ class MainFeedViewController: UIViewController, UITableViewDelegate, UITableView
     
     var singlesSelected : Bool = false
     let baseURL : String = "http://ec2-35-183-247-114.ca-central-1.compute.amazonaws.com"
+    var feedReusableCell = ""
+    var feedJSON : JSON!
     var feedImages : Image!
-    var singlesArray = [UserModel]()
-    var couplesArray = [CouplesUserModel]()
     var selectedUserID = ""
     let numberUserLoaded = 15
-    var feedArraysFilled = false
     var userToken = UserData().defaults.string(forKey: "userToken")
     var feedOffset = 0
 
@@ -71,17 +70,14 @@ class MainFeedViewController: UIViewController, UITableViewDelegate, UITableView
         //giving basic value of true to singlesSelected so that it is the default tab shown when loading this view
         singlesSelected = false
         
-        //giving singlesArrayFilled a default value of false
-        feedArraysFilled = false
-        
         // call some kind of function that loads singles data --> dataLoaded() or loadData()
         configureTableView()
         
         //we make sure feedoffset is = to 0
         feedOffset = 0
-        
         //get user data
         getFeed()
+        
     }//end of viewDidLoad
     
     
@@ -108,39 +104,41 @@ class MainFeedViewController: UIViewController, UITableViewDelegate, UITableView
     
     //MARK: - prefetchRowsAt
     func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
-        
+        //feedOffset += numberUserLoaded
+        //getFeed()
     }//end of prefetchRowsAt
     
     // MARK: - Declare cellForRowAT
+    // declare cellForRowAt func
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // if that changes the custom cell depending on what segment is choosen ( Singles or couples)
+        //let messageArray = ["test 12","Hello world", "Live long and prosper", "Hamza Khan", "Logan Mack"]
         if singlesSelected == true {
-            //if that checks if the index path if equal to the #of users in the singlesArray and if so loads another 15 users
-            if indexPath.row == singlesArray.count - 1 {
-                print("loading next page")
-                feedOffset += 15
-                print("Feed offset: \(feedOffset)")
-                getRemainingFeed()
+            print(feedJSON["result"]["single"].count)
+            if indexPath.row == feedJSON["result"]["single"].count {
+                print("if index path is equal to feed.count")
+                getFeed()
             }//end of if
-            
+            print("singlesSelected")
+//            let imageURL = feedJSON["result"]["unmatched"][indexPath.row]["ProfilePicture"].string
 //            loadUserPicture(pictureURL: imageURL!)
-            
-            print("singlesArray.count: \(singlesArray.count)")
             
             // initialization of cell which is the var with the custom cell
             let cell = tableView.dequeueReusableCell(withIdentifier: "singlesCell", for: indexPath) as! SinglesTableViewCell
-            
+            cell.nameLabel.text = feedJSON["result"]["single"][indexPath.row]["FirstName"].string
+            cell.gradeLabel.text = "Grade: \(feedJSON["result"]["single"][indexPath.row]["Grade"])"
+            cell.bioLabel.text = feedJSON["result"]["single"][indexPath.row]["Biography"].string
             cell.avatarImageView.image = UIImage(named: "avatar_placeholder")
-            
-            cell.nameLabel.text = singlesArray[indexPath.row].userFirstName
-            cell.gradeLabel.text = "Grade: \(singlesArray[indexPath.row].userGrade)"
-            cell.bioLabel.text = singlesArray[indexPath.row].userBio
+            //let imageURL = feedJSON["result"]["unmatched"][indexPath.row]["ProfilePicture"].string
+            //cell.avatarImageView.image = loadUserPicture(pictureURL: imageURL!)
+            //cell.avatarImageView.image = feedImages
             
             return cell
         } else {
             let cell = tableView.dequeueReusableCell(withIdentifier: "couplesCell", for: indexPath) as! CouplesTableViewCell
-            cell.couplesNamesLabel.text = "\(couplesArray[indexPath.row].userFirstName) & \(couplesArray[indexPath.row].partnerFirstName)"
-            
+            cell.couplesNamesLabel.text = "\(feedJSON["result"]["couple"][indexPath.row][0]["FirstName"]) & \(feedJSON["result"]["couple"][indexPath.row][1]["FirstName"])"
+            //let couplesArray = ["El & Sam", "Lucas & Max", "Mike & Eleven", "t'pol & tripp", "Picard & Crusher", "El & Sam", "El & Sam", "El & Sam", "El & Sam", "El & Sam", "El & Sam"]
+            //cell.couplesNamesLabel.text = couplesArray[indexPath.row]
             cell.firstAvatarImageView.image = UIImage(named: "avatar_placeholder")
             cell.seccondAvatarImageView.image = UIImage(named: "avatar_placeholder")
             return cell
@@ -151,26 +149,27 @@ class MainFeedViewController: UIViewController, UITableViewDelegate, UITableView
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         //return 11
         print("entered numbersOfRowsInSection")
-        if feedArraysFilled == true {
-            if singlesSelected == true{
-                return singlesArray.count
+        print(feedJSON)
+        if feedJSON != nil {
+            if singlesSelected == true {
+                return feedJSON["result"]["single"].count
             } else {
-                return couplesArray.count
+                return feedJSON["result"]["couple"].count
             }//end of if/else
         } else {
             return 0
-        }//end of if/else
+        }
     }// end of numbersOfRowsInSection
     
     //MARK: - didSelectRowAt function
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if singlesSelected == true {
-            selectedUserID = singlesArray[indexPath.row].id
+            selectedUserID = feedJSON["result"]["single"][indexPath.row]["ID"].string!
             performSegue(withIdentifier: "goToSelectedUser", sender: self)
         }// end of if
         
         if singlesSelected == false {
-            selectedUserID = couplesArray[indexPath.row].userID
+            selectedUserID = feedJSON["result"]["couple"][indexPath.row][0]["ID"].string!
             performSegue(withIdentifier: "goToSelectedCouple", sender: self)
         }// end of if
     }// end of didSelectRowAt
@@ -180,65 +179,19 @@ class MainFeedViewController: UIViewController, UITableViewDelegate, UITableView
         print(feedOffset)
         let callURL = baseURL + "/php/feed.new.php"
         let params : [String : Any] = ["token" : userToken!, "max-size" : numberUserLoaded, "single-offset" : feedOffset]
-        
         Alamofire.request(callURL, method: .get, parameters: params).responseJSON {
             response in
             if response.result.isSuccess {
                 print("sucess got data")
-                let responseJSON : JSON = JSON(response.result.value!)
-                print(responseJSON)
-                self.processFeedData(feedJSON: responseJSON)
+                self.feedJSON = JSON(response.result.value!)
+                print(self.feedJSON)
+                self.feedTableView.reloadData()
             } else {
                 print("Failed to get Data : there was an error during the request")
                 print("error: \(response.result.error!)")
             }//end of if/else
         }// end of request
     }// end of getFeed
-    
-    func processFeedData(feedJSON : JSON) {
-        print("processFeedData entered")
-        print("Singles Selected: \(singlesSelected)")
-        
-        print("if singles entered")
-        //for loop for the singlesData
-        print("feedJSON.count \(feedJSON["result"]["single"].count)")
-        if feedJSON["result"]["single"].count >= 1 {
-            for index in 0...feedJSON["result"]["single"].count - 1 {
-                singlesArray.append(UserModel(gender: feedJSON["result"]["single"][index]["Gender"].string!, grade: feedJSON["result"]["single"][index]["Grade"].string!, lastName: feedJSON["result"]["single"][index]["LastName"].string!, schoolID: feedJSON["result"]["single"][index]["SchoolID"].string!, bio: feedJSON["result"]["single"][index]["Biography"].string!, userID: feedJSON["result"]["single"][index]["ID"].string!, firstName: feedJSON["result"]["single"][index]["FirstName"].string!))
-            }//end for loop
-            if feedJSON["result"]["couple"].count >= 1 {
-                for index in 0...feedJSON["result"]["couple"].count - 1 {
-                    couplesArray.append(CouplesUserModel(usrSchoolID: feedJSON["result"]["couple"][index][0]["SchoolID"].string!, usrFirstName: feedJSON["result"]["couple"][index][0]["FirstName"].string!, usrLastName: feedJSON["result"]["couple"][index][0]["LastName"].string!, usrBio: feedJSON["result"]["couple"][index][0]["Biography"].string!, usrGender: feedJSON["result"]["couple"][index][0]["Gender"].string!, usrID: feedJSON["result"]["couple"][index][0]["ID"].string!, usrGrade: feedJSON["result"]["couple"][index][0]["Gender"].string!, prtnSchoolID: feedJSON["result"]["couple"][index][1]["SchoolID"].string!, prtnFirstName: feedJSON["result"]["couple"][index][1]["FirstName"].string!, prtnLastName: feedJSON["result"]["couple"][index][1]["LastName"].string!, prtnBio: feedJSON["result"]["couple"][index][1]["Biography"].string!, prtnGender: feedJSON["result"]["couple"][index][1]["Gender"].string!, prtnID: feedJSON["result"]["couple"][index][1]["ID"].string!, prtnGrade: feedJSON["result"]["couple"][index][1]["Grade"].string!))
-                }//end of for loop
-            }//end of if
-            //self.feedOffset += self.numberUserLoaded
-            print("end of forloop")
-            feedArraysFilled = true
-            self.feedTableView.reloadData()
-        }//end of if
-    }//end of processFeedData
-    
-    
-    func getRemainingFeed() {
-        print("getRemainingFeed")
-        print(feedOffset)
-        let callURL = baseURL + "/php/feed.new.php"
-        let params : [String : Any] = ["token" : userToken!, "max-size" : numberUserLoaded, "single-offset" : feedOffset]
-        
-        Alamofire.request(callURL, method: .get, parameters: params).responseJSON {
-            response in
-            if response.result.isSuccess {
-                print("sucess got data")
-                let responseJSON : JSON = JSON(response.result.value!)
-                print(responseJSON)
-                self.processFeedData(feedJSON: responseJSON)
-            } else {
-                print("Failed to get Data : there was an error during the request")
-                print("error: \(response.result.error!)")
-            }//end of if/else
-        }// end of request
-    }
-    
     
     //MARK: - Declare configureTableView
     //configureTableView is a func which allows the tableViewCells to have the good rowHeight so that the custom cells will fit properly
